c6333d5d67e6b6454870dd3b461d59ac814c2b51
Max Cardillo <mcardillo@mitre.org>
2022-06-03 14:04:05 -0400

    Testing creation of menu in rust file.

---
+++ src-tauri/src/test.rs
@@ -0,0 +1,19 @@
+fn create_global_menus {
+  // Create the global application menus
+  /* use tauri::{CustomMenuItem, Menu, MenuItem, Submenu};
+
+  let quit = CustomMenuItem::new("quit".to_string(), "Quit");
+  let close = CustomMenuItem::new("close".to_string(), "Close");
+  let save = CustomMenuItem::new("save".to_string(), "Save");
+  let file_submenu = Submenu::new("File", Menu::new().add_item(save).add_item(quit).add_item(close));
+
+  let reload = CustomMenuItem::new("reload".to_string(), "Reload");
+  let tools = CustomMenuItem::new("devtools".to_string(), "Show Developer Tools");
+  let edit_submenu = Submenu::new("Edit", Menu::new().add_item(reload).add_item(tools));
+
+  let menu = Menu::new()
+    .add_native_item(MenuItem::Copy)
+    .add_item(CustomMenuItem::new("hide", "Hide"))
+    .add_submenu(file_submenu);
+    .add_submenu(edit_submenu); */
+}
